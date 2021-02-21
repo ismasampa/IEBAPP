74912ba07c486b6cd283bf42543a796e5e8e412e
ismasampa <ismael.santos@fato-ti.com.br>
2021-02-21 04:03:09 -0300

    prod environment first ajust

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://localhost/ieventbook/",
+  apiUrlproxyed: "http://localhost/ieventbook/",
+  userAuth: "IsmaelRibeiro",
+  JUNO_CLIENT_ID:"0MofIktZ1QNRiVBF",
+  JUNO_SECRET:"oY|h_(<hyff#V7v8pT)72NA1q[{6#M3z",
+  JUNO_TOKEN:"899F73CEC229B80B1616838302BE3F2D5152DD95D9B91058B2705BD35F545411",
+  JUNO_ENV:"dev"
 };
