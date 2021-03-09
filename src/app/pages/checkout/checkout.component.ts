29d5ba11ffcbd79427f208f8a6e2f07a53a92a03
ismasampa <ismael.santos@fato-ti.com.br>
2021-03-09 19:51:55 -0300

    header reformatting

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { JunoService } from 'src/app/services/juno.service';
+
+declare var window;
+declare var checkout;
 @Component({
 
   selector: 'app-checkout',
@@ -8,15 +11,33 @@ import { JunoService } from 'src/app/services/juno.service';
 })
 export class CheckoutComponent implements OnInit {
   checkout: any;
+  @ViewChild("cardHash") cardHash: ElementRef;
+  @ViewChild("cardHashError") cardHashError: ElementRef;
+  
   constructor(private junoservice: JunoService ) { 
+    
   }
 
   ngOnInit(): void {
     };
 
     recebeHash() {     
-      window.generateHash(); 
+      var cardData = {
+        cardNumber: '5500497511776173',
+        holderName: 'Nome do Titular do Cartão',
+        securityCode: '000',
+        expirationMonth: '12',
+        expirationYear: '2045'
+      };
+      console.log("comeco generate");
+
+      window.generateHash(cardData, this.retorno); 
+      console.log("fim generate");
+
   };
 
-
+  retorno(hash,erro){
+    this.cardHash = hash;
+    this.cardHashError = erro;
+  };
 }
