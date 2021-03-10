e2d9dfe123f3793bd10a24e309dd9bf84fb3c202
ismasampa <ismael.santos@fato-ti.com.br>
2021-03-10 09:38:24 -0300

    Ajustes de navegação e caching do market

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -5,6 +5,7 @@ import { ShopService } from 'src/app/services/shop.service';
 import { takeUntil } from 'rxjs/operators';
 import { Item } from 'src/app/models/Item';
 import { Observable } from 'rxjs';
+import { isIndexSignatureDeclaration } from 'typescript';
 
 @Component({
 
@@ -15,7 +16,18 @@ import { Observable } from 'rxjs';
 export class CheckoutComponent implements OnInit {
   checkout: any;
   modalRef: BsModalRef;
-  items: any =  [{ "product": "Fralda RN 10", "description": "para sujar", "valor": 10.2 }, { "product": "Fralda 2", "description": "para sujar tb", "valor": 20.1 }];
+  items: any =  [
+  { "product": "Fralda RN", "description": "RN 10", "value": 10, "qtd": 0 }, 
+  { "product": "Fralda P", "description": "P 10", "value": 20, "qtd": 0 }, 
+  { "product": "Fralda M", "description": "M 10", "value": 30, "qtd": 0 }, 
+  { "product": "Fralda G", "description": "G 10", "value": 40, "qtd": 0 }, 
+  { "product": "Fralda XG", "description": "XG 10", "value": 50, "qtd": 0 }, 
+  { "product": "Fralda XXG", "description": "XXG 10", "value": 60, "qtd": 0 }, 
+  { "product": "Fralda G30", "description": "G 30", "value": 80, "qtd": 0 }, 
+  { "product": "Fralda XG30", "description": "XXG 30", "value": 100, "qtd": 0 }, 
+  { "product": "Fralda c/ alarme", "description": "Alerta", "value": 200, "qtd": 0 }, 
+  { "product": "Fralda Geriátrica", "description": "Geriat.", "value": 300, "qtd": 0 }
+  ];
   unsubscribe$: Observable<any>;
   cart: Item[];
   step:number;
@@ -28,6 +40,15 @@ export class CheckoutComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.initCart();
+  }
+
+  initCart() {
+    this.cart.forEach( x=>{
+      if(x.qtd){
+        this.items.filter(z=>z.product==x.product)[0].qtd = x.qtd;
+      }
+    });
   }
 
   openModal(template: TemplateRef<any>) {
@@ -50,6 +71,7 @@ export class CheckoutComponent implements OnInit {
       }
       this.shopService.delItem(del);
   }
+
   recebeHash() {
     this.junoCardService.cript();
   }
